a4f777ae42de473b00fdcf78f4cde9fe93d77973
Generosa Litton <glcodeworks@gmail.com>
2021-10-06 14:54:05 -0700

    dynamic pages using gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,52 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const storyblokEntry = path.resolve("src/templates/page.js")
+
+    resolve(
+      graphql(
+        `
+          {
+            stories: allStoryblokEntry(
+              filter: { field_component: { eq: "page" } }
+            ) {
+              edges {
+                node {
+                  id
+                  name
+                  slug
+                  field_component
+                  full_slug
+                  content
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const entries = result.data.stories.edges
+
+        entries.forEach(entry => {
+          if (entry.slug !== "home") {
+            const page = {
+              path: `/${entry.node.full_slug}`,
+              component: storyblokEntry,
+              context: {
+                story: entry.node,
+              },
+            }
+            createPage(page)
+          }
+        })
+      })
+    )
+  })
+}
